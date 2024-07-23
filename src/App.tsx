ea1447da12d67f7ee4820a777ee5e625b4a1834d
elsacodesid <elsacodesid@gmail.com>
2024-07-23 09:43:59 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,48 @@
-import { useState } from 'react'
-import words from "./wordList.json"
+import { useState } from "react";
+import words from "./wordList.json";
 
-import './App.css'
-import HangmanDrawing from './HangmanDrawing.tsx'
-import HangmanWord from './HangmanWord.tsx'
-import Keyboard from "./Keyboard.tsx"
+import "./App.css";
+import HangmanDrawing from "./HangmanDrawing.tsx";
+import HangmanWord from "./HangmanWord.tsx";
+import Keyboard from "./Keyboard.tsx";
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
-return words[Math.floor(Math.random() * words.length)]
-  })
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
-  const [count, setCount] = useState(0)
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  const [count, setCount] = useState(0);
 
   return (
-  <div style={{
-    maxWidth: "800px",
-    display: "flex",
-    flexDirection: "column",
-    gap: "2rem",
-    margin: "0 auto",
-    alignItems: "center"
-  }}>
-<div style={{
-  fontSize: "2rem",
-  textAlign: "center"
-}}>
-  Lose
-  Win
-</div>
-<HangmanDrawing />
-<HangmanWord />
-<Keyboard />
-  </div>
-  )
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          fontSize: "2rem",
+          textAlign: "center",
+        }}
+      >
+        Lose Win
+      </div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <div
+        style={{
+          alignSelf: "stretch",
+        }}
+      >
+        <Keyboard />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+const word = "test"
+const guessedLetters = ["t"]
 function HangmanWord() {
   return (
-    <div>HangmanWord</div>
+    < div style={{
+      display: "flex",
+      gap: ".25em",
+      fontSize: "6rem",
+      fontWeight: "bold",
+      textTransform: "uppercase", 
+      fontFamily: "monospace"
+    }}>{word.split("").map((letter, index) => (
+      <span style={{
+        borderBottom: ".1em solid white"
+      }}><span key={index} style={{
+        visibility: guessedLetters.includes(letter) ? "visible" : "hidden",
+
+      }}>{letter}</span></span>
+    ))}</div>
   )
 }
 
